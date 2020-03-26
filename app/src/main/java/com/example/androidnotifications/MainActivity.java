92fe93f6f77448b4ef92d9fa8bb862c4c27d3ec6
ktmshyamal <ktmshyamal@gmail.com>
2020-03-27 00:08:12 +0530

    Everything is done

--- app/src/main/java/com/example/androidnotifications/MainActivity.java
+++ app/src/main/java/com/example/androidnotifications/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.androidnotifications;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,45 +15,44 @@ import androidx.core.app.NotificationManagerCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String CHANNEL_ID = "notification";
+    private static final String CHANNEL_ID = String.valueOf(0);
 
-    Button sign;
-    EditText value;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        sign = findViewById(R.id.id_btn_sign);
 
-        sign.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "AndroidNotification";
+            String description = "androidNotification";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription (description); // Register the channel with the system; you can't change the importance or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
 
-                //get user input(Name) to String variable
-                value = findViewById(R.id.id_name);
-                String name = value.getText().toString();
-
-                // Create an explicit intent for an Activity in your app
-                Intent intent = new Intent(MainActivity.this,register.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-
-                PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, 0, intent, 0);
-
-                //set user name (Hello << name >>! Welcome to the MAD team)
-                //image
-                NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this, CHANNEL_ID)
-                        .setSmallIcon(R.drawable.ic_launcher_background)
-                        .setContentTitle("Click on this to Register")
-                        .setContentText("Hello " + name + " ! Welcome to the MAD team")
-                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                        .setContentIntent(pendingIntent)
-                        .setAutoCancel(true);
-
-                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(MainActivity.this);
-
-                notificationManager.notify(0, builder.build());
-            }
-        });
     }
+
+    public void SIGNUP(View view) {
+
+        EditText text = (EditText) findViewById(R.id.id_name);
+        String message = text.getText().toString();
+
+        // Create an explicit intent for an Activity in your app
+        Intent intent = new Intent(this, register.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("Notification from My App...!" )
+                .setContentText("Hello " + message)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT) // Set the intent that will fire when the user taps the notification
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this); // notificationId is a unique int for each notification that you must define
+        notificationManager.notify(0, builder.build());
+    }
+
 }
